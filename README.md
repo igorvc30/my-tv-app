3e63d89ed90a1e650f9aca70752a48797ca34949
Igor V Costa <igor.vc30@gmail.com>
2025-11-04 13:06:35 -0300

    chore: initial project setup

---
+++ README.md
@@ -0,0 +1,16 @@
+# My TV
+
+## This project is using
+
+1. React + TypeScript
+2. Vite
+3. ESLint + prettier
+4. Material UI + emotion + scss
+5. Tanstack Query
+
+## How to run
+
+Use the following commands
+
+1. `npm install`
+2. `npm run dev`
---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "./index.scss";
+import App from "./App.tsx";
+
+// Create a Query Client
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // Optional default
+      staleTime: 5 * 60 * 1000, // Optional default (e.g., 5 minutes)
+    },
+  },
+});
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} /> {/* Optional devtools */}
+    </QueryClientProvider>
+  </StrictMode>,
+);
